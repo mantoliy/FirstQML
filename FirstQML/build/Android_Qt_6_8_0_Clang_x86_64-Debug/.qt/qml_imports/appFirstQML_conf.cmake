6a5834c58aeb2c12c12d7eb1d4dadb1b4dd0fcfe
Manuel Anatoliy Arcia Salmeron <mantoliy@hotmail.com>
2024-10-23 13:03:25 -0600

    Adde a button and file menu

---
+++ FirstQML/build/Android_Qt_6_8_0_Clang_x86_64-Debug/.qt/qml_imports/appFirstQML_conf.cmake
@@ -0,0 +1,7 @@
+set(qml_import_scanner_imports_count 5)
+set(qml_import_scanner_import_0 "CLASSNAME;QtQuick2Plugin;LINKTARGET;Qt6::qtquick2plugin;NAME;QtQuick;PATH;/home/anatoliy/Qt/6.8.0/android_x86_64/qml/QtQuick;PLUGIN;qml_QtQuick_qtquick2plugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQuick/;RELATIVEPATH;QtQuick;TYPE;module;")
+set(qml_import_scanner_import_1 "CLASSNAME;QtQmlPlugin;LINKTARGET;Qt6::qmlplugin;NAME;QtQml;PATH;/home/anatoliy/Qt/6.8.0/android_x86_64/qml/QtQml;PLUGIN;qml_QtQml_qmlplugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQml/;RELATIVEPATH;QtQml;TYPE;module;")
+set(qml_import_scanner_import_2 "NAME;QML;PATH;/home/anatoliy/Qt/6.8.0/android_x86_64/qml/QML;PREFER;:/qt-project.org/imports/QML/;RELATIVEPATH;QML;TYPE;module;")
+set(qml_import_scanner_import_3 "CLASSNAME;QtQmlModelsPlugin;LINKTARGET;Qt6::modelsplugin;NAME;QtQml.Models;PATH;/home/anatoliy/Qt/6.8.0/android_x86_64/qml/QtQml/Models;PLUGIN;qml_QtQml_Models_modelsplugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQml/Models/;RELATIVEPATH;QtQml/Models;TYPE;module;")
+set(qml_import_scanner_import_4 "CLASSNAME;QtQmlWorkerScriptPlugin;LINKTARGET;Qt6::workerscriptplugin;NAME;QtQml.WorkerScript;PATH;/home/anatoliy/Qt/6.8.0/android_x86_64/qml/QtQml/WorkerScript;PLUGIN;qml_QtQml_WorkerScript_workerscriptplugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQml/WorkerScript/;RELATIVEPATH;QtQml/WorkerScript;TYPE;module;")
+
